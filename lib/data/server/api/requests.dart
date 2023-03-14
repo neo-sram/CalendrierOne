3120b7abe4f520a6a0aa8b834ff7ef1d8acfe32f
neo-sram <tahamakhlouf234@gmail.com>
2023-03-15 00:33:09 +0100

    branche : api_model => changed request file to have the response code in the methods

--- lib/data/server/api/requests.dart
+++ lib/data/server/api/requests.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: avoid_print, unused_import
+// ignore_for_file: avoid_print, unused_import, empty_catches
 
 import 'package:dio/dio.dart';
 import 'dart:convert';
@@ -9,45 +9,48 @@ class ApiRequests {
     BaseOptions(
       baseUrl: ApiConsts.url,
       responseType: ResponseType.plain,
+      receiveDataWhenStatusError: true,
+      headers: {
+        'lang': 'eng',
+        'Content-Type': 'application/json',
+      },
     ),
   );
 
   ApiRequests() {
     _dio;
-    interceptorsInit();
   }
 
-  interceptorsInit() {
-    _dio.interceptors.add(
-      InterceptorsWrapper(
-        onError: (e, errorHandler) {
-          print(e.message);
-        },
-        onRequest: (request, requestHandler) {
-          print('${request.method} ${request.path}');
-        },
-        onResponse: (responce, responceHandler) {
-          print('${responce.data}');
-        },
-      ),
-    );
+  static Future<Response?> getAllCalender() async {
+    try {
+      final responseAll =
+          await _dio.get(ApiConsts.url + ApiConsts.getAllDataRequest);
+      if (responseAll.statusCode == 200) {
+        return responseAll;
+      }
+    } catch (e) {}
+    return null;
   }
 
-  Future<Response> getAllCalender() async {
-    final responseAll =
-        await _dio.get(ApiConsts.url + ApiConsts.getAllDataRequest);
-    return responseAll;
+  static Future<Response?> getDayByID({required int id}) async {
+    try {
+      final responceId =
+          await _dio.get(ApiConsts.url + ApiConsts.getDataById + id.toString());
+      if (responceId.statusCode == 200) {
+        return responceId;
+      }
+    } catch (e) {}
+    return null;
   }
 
-  Future<Response> getDayByID({required int id}) async {
-    final responceId =
-        await _dio.get(ApiConsts.url + ApiConsts.getDataById + id.toString());
-    return responceId;
-  }
-
-  Future<Response> getDayByDay({required String dayName}) async {
-    final responceDay =
-        await _dio.get(ApiConsts.url + ApiConsts.getDataByDayName + dayName);
-    return responceDay;
+  static Future<Response?> getDayByDay({required String dayName}) async {
+    try {
+      final responceDay =
+          await _dio.get(ApiConsts.url + ApiConsts.getDataByDayName + dayName);
+      if (responceDay.statusCode == 200) {
+        return responceDay;
+      }
+    } catch (e) {}
+    return null;
   }
 }
